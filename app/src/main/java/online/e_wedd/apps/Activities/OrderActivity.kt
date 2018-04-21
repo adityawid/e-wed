d5bf306634d96477bba95c6e4b10617dce229ce2
Aditya Widayanto <adityawi2590@gmail.com>
2018-04-21 14:53:21 +0700

    init project

---
+++ app/src/main/java/online/e_wedd/apps/Activities/OrderActivity.kt
@@ -0,0 +1,13 @@
+package online.e_wedd.apps.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import online.e_wedd.apps.R
+
+class OrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
